65a10882adb54dc43b91ab70a373bfa85e45c703
jharte5 <jonathan.harte@codeimmersives.com>
2019-11-18 14:08:57 -0500

    first half

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 function getFirstName(person) {
+  return person.firstName
 }
 
 function getLastName(person) {
+  return person.lastName
 }
 
 function getFullName(person) {
+  return person.firstName + ' ' + person.lastName
 }
 
 function setFirstName(person, name) {
+  return person.firstName = name
 }
 
 function setAge(person, age) {
+  newAge = {age: 29};
+  return person.newAge
 }
 
 function giveBirthday(person) {
+  return 
 }
 
 function marry(person1, person2) {
